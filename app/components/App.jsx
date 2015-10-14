d0b07e41643f8302cb66af2a131fed26c5b92e21
papakraken <parents.of.the.corn@icloud.com>
2015-10-14 16:42:05 +0200

    1. Faux create task implemented.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,27 +1,47 @@
 import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
-const notes = [
-	{
-		id: uuid.v4(),
-		task: 'Learn Webpack'
-	},
-	{
-		id: uuid.v4(),
-		task: 'Learn React'
-	},
-	{
-		id: uuid.v4(),
-		task: 'Do laundry'
-	}
-];
 
 export default class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			notes: [
+				{
+					id: uuid.v4(),
+					task: 'Learn Webpack'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Learn React'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Do laundry'
+				}
+			]
+		};
+		
+		this.addNote = this.addNote.bind(this);
+	}	
+	
 	render() {
+		const notes = this.state.notes;
 		return (
 			<div>
+				<button className="add-note" onClick={this.addNote}>+</button>
 				<Notes items={notes} />
 			</div>
 		);
+	}
+	
+	addNote(){
+		console.log('add note');
+		this.setState({
+			notes: this.state.notes.concat([{
+				id: uuid.v4(),
+				task: 'New task'
+			}])
+		});
 	}
 }
