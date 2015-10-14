5fe2edc065cc2ba38492f209b7deccdda2f463d3
papakraken <parents.of.the.corn@icloud.com>
2015-10-14 19:02:33 +0200

    CRUD complete on Basic notes!

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -22,7 +22,10 @@ export default class App extends React.Component {
 			]
 		};
 		
+		this.findNote = this.findNote.bind(this);
 		this.addNote = this.addNote.bind(this);
+		this.editNote = this.editNote.bind(this);
+		this.deleteNote = this.deleteNote.bind(this);
 	}	
 	
 	render() {
@@ -30,7 +33,9 @@ export default class App extends React.Component {
 		return (
 			<div>
 				<button className="add-note" onClick={this.addNote}>+</button>
-				<Notes items={notes} />
+				<Notes items={notes} 
+					onEdit={this.editNote}
+					onDelete={this.deleteNote} />
 			</div>
 		);
 	}
@@ -43,5 +48,47 @@ export default class App extends React.Component {
 				task: 'New task'
 			}])
 		});
+	}
+	
+	editNote(id, task){
+		let notes = this.state.notes;
+		const noteIndex = this.findNote(id);
+		
+		if(noteIndex < 0) {
+			return;
+		}
+		
+		notes[noteIndex].task = task;
+		
+		this.setState({notes});
+		
+		console.log('note edited', id, task);
+	}
+	
+	deleteNote(id) {
+		const notes = this.state.notes;
+		const noteIndex = this.findNote(id);
+		
+		if(nodeIndex < 0) {
+			return;
+		}
+		
+		this.setState({
+			notes: notes.slice(0,noteIndex).concat(notes.slice(noteIndex +1))
+		});
+	
+	}
+	
+	findNote(id) {
+	
+		const notes = this.state.notes;
+		const noteIndex = notes.findIndex((note) => note.id === id);
+		
+		if(noteIndex < 0) {
+			console.warn('Failed to find note', notes, id);
+		}
+		
+		return noteIndex;
+	
 	}
 }
