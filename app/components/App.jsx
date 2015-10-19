348c8b6256cad2f6d5785415dccbf6cfc4360afe
papakraken <parents.of.the.corn@icloud.com>
2015-10-19 15:02:17 +0200

    1. Lanes and Notes combined to Kanban

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,96 +1,26 @@
-import uuid from 'node-uuid';
+import AltContainer from 'alt/AltContainer';
 import React from 'react';
-import Notes from './Notes.jsx';
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
 
 export default class App extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			notes: [
-				{
-					id: uuid.v4(),
-					task: 'Learn Webpack'
-				},
-				{
-					id: uuid.v4(),
-					task: 'Learn React'
-				},
-				{
-					id: uuid.v4(),
-					task: 'Do laundry'
-				}
-			]
-		};
-		
-		this.findNote = this.findNote.bind(this);
-		this.addNote = this.addNote.bind(this);
-		this.editNote = this.editNote.bind(this);
-		this.deleteNote = this.deleteNote.bind(this);
-	}	
-	
-	render() {
-		const notes = this.state.notes;
-		return (
-			<div>
-				<button className="add-note" onClick={this.addNote}>+</button>
-				<Notes items={notes} 
-					onEdit={this.editNote}
-					onDelete={this.deleteNote} />
-			</div>
-		);
-	}
-	
-	addNote(){
-		console.log('add note');
-		this.setState({
-			notes: this.state.notes.concat([{
-				id: uuid.v4(),
-				task: 'New task'
-			}])
-		});
-	}
-	
-	editNote(id, task){
-		let notes = this.state.notes;
-		const noteIndex = this.findNote(id);
-		
-		if(noteIndex < 0) {
-			return;
-		}
-		
-		notes[noteIndex].task = task;
-		
-		this.setState({notes});
-		
-		console.log('note edited', id, task);
-	}
-	
-	deleteNote(id) {
-		const notes = this.state.notes;
-		const noteIndex = this.findNote(id);
-		
-		if(noteIndex < 0) {
-			return;
-		}
-		
-		this.setState({
-			notes: notes.slice(0,noteIndex).concat(notes.slice(noteIndex +1))
-		});
-		
-		
-	
-	}
-	
-	findNote(id) {
-	
-		const notes = this.state.notes;
-		const noteIndex = notes.findIndex((note) => note.id === id);
-		
-		if(noteIndex < 0) {
-			console.warn('Failed to find note', notes, id);
-		}
-		
-		return noteIndex;
-	
-	}
+  render() {
+    return (
+      <div>
+        <button className="add-lane" onClick={this.addItem}>+</button>
+        <AltContainer
+          stores={[LaneStore]}
+          inject={ {
+            items: () => LaneStore.getState().lanes || []
+          } }
+        >
+          <Lanes />
+        </AltContainer>
+      </div>
+    );
+  }
+  addItem() {
+    LaneActions.create({name: 'New lane'});
+  }
 }
